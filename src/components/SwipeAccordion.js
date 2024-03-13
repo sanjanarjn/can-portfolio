60b4c2fe3b2476e6ce1f78b80ec334eaadc7486d
sanjanarjn <sanjanarjn@gmail.com>
2024-03-12 23:42:28 -0500

    Sipe accordion added for projects

--- src/components/SwipeAccordion.js
+++ src/components/SwipeAccordion.js
@@ -4,26 +4,29 @@ import { Swiper, SwiperSlide } from "swiper/react";
 
 // Import Swiper styles
 import "swiper/css";
-import 'swiper/css/pagination';
+import "swiper/css/pagination";
 
-import { Autoplay, Pagination, Navigation } from 'swiper/modules';
+import { Autoplay, Pagination, Navigation } from "swiper/modules";
 import ProjectDetail from "./ProjectDetail";
 
 import pantryPalImage from "../images/pantry-pal.png";
 import coRideImage from "../images/co-ride.png";
 
 function SwipeAccordion() {
+  const pantryPalSubheading =
+    "Simple extension which simplifies your weekly meal planning and groceries! ";
+  const pantryPalDescription =
+    "PantryPal is a straightforward meal planning and grocery list app designed for simplicity. It helps you organize your weekly meals and shopping lists.";
 
-  const pantryPalSubheading = "Simple extension which simplifies your weekly meal planning and groceries! "
-  const pantryPalDescription = "PantryPal is a straightforward meal planning and grocery list app designed for simplicity. It helps you organize your weekly meals and shopping lists."
-
-  const coRideSubHeading = "Simple extension which simplifies your weekly meal planning and groceries! "
-  const coRideDescription = "PantryPal is a straightforward meal planning and grocery list app designed for simplicity. It helps you organize your weekly meals and shopping lists."
+  const coRideSubHeading =
+    "Simple extension which simplifies your weekly meal planning and groceries! ";
+  const coRideDescription =
+    "PantryPal is a straightforward meal planning and grocery list app designed for simplicity. It helps you organize your weekly meals and shopping lists.";
 
   return (
     <>
       <Swiper
-        slidesPerView={'auto'}
+        slidesPerView={"auto"}
         centeredSlides={true}
         spaceBetween={30}
         pagination={{
@@ -33,15 +36,41 @@ function SwipeAccordion() {
         modules={[Autoplay, Pagination, Navigation]}
         className="items-center"
       >
-        <SwiperSlide><ProjectDetail title="PANTRY PAL" image={pantryPalImage} subheading={pantryPalSubheading} description={pantryPalDescription}/></SwiperSlide>
-        <SwiperSlide><ProjectDetail title="CO-RIDE" image={coRideImage} subheading={coRideSubHeading} description={coRideDescription}/></SwiperSlide>
-      <SwiperSlide><ProjectDetail title="PANTRY PAL" image={pantryPalImage} subheading={pantryPalSubheading} description={pantryPalDescription}/></SwiperSlide>
-        <SwiperSlide><ProjectDetail title="CO-RIDE" image={coRideImage} subheading={coRideSubHeading} description={coRideDescription}/></SwiperSlide>
-
+        <SwiperSlide>
+          <ProjectDetail
+            title="PANTRY PAL"
+            image={pantryPalImage}
+            subheading={pantryPalSubheading}
+            description={pantryPalDescription}
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <ProjectDetail
+            title="CO-RIDE"
+            image={coRideImage}
+            subheading={coRideSubHeading}
+            description={coRideDescription}
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <ProjectDetail
+            title="PANTRY PAL"
+            image={pantryPalImage}
+            subheading={pantryPalSubheading}
+            description={pantryPalDescription}
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <ProjectDetail
+            title="CO-RIDE"
+            image={coRideImage}
+            subheading={coRideSubHeading}
+            description={coRideDescription}
+          />
+        </SwiperSlide>
       </Swiper>
     </>
   );
 }
-
 
 export default SwipeAccordion;
